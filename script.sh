06c640e1d5c1b87e1aeebd79a4eb8c38777f1445
Ubuntu <ubuntu@ip-172-31-7-40.eu-west-2.compute.internal>
2023-03-19 13:09:26 +0000

    had to install kubectl

--- script.sh
+++ script.sh
@@ -25,3 +25,9 @@ sudo yum -y install terraform
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
 
+#install kubectl
+
+sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl
+sudo chmod +x ./kubectl
+sudo mkdir -p $HOME/bin && sudo cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+
